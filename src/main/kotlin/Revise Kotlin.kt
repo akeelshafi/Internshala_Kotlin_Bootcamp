e857bd090989fb23a9ef01d2b05cc44b336d52a9
Akeel shafi <140949439+akeelshafi@users.noreply.github.com>
2024-08-03 09:59:16 +0530

    Day 7 of kotlin bootcamp

---
+++ src/main/kotlin/Revise Kotlin.kt
@@ -0,0 +1,64 @@
+// main function in kotlin
+fun main(){
+
+    // comments in kotlin
+     /* multi line comment                 */
+
+    // hello world program
+
+    // print statement
+    println("Hello world!") // with new line
+    print("Hello world!") // without new line
+
+
+    //Operators in kotlin
+    /*
+    Ar-thematic operators
+    +
+    -
+    *
+    /
+    %
+
+    Assignment operators
+    =
+    +=
+    -=
+    *=
+    /=
+    %=
+
+   Logical operators
+   && and
+   || OR
+   !  NOT
+
+
+   Comparison operators
+   ==
+   <
+   <=
+   >
+   >=
+
+
+
+   DATA TYPES IN KOTLIN
+
+   int
+   float
+   double
+   string
+   char
+   boolean
+
+   without mention of data type == unit
+
+
+
+
+
+    */
+
+
+}
