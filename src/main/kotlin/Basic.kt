b18fae9edb13c1aa29e1a3979cd0de9bd39eef29
Akeel shafi <akeelshafi20@gmail.com>
2024-10-13 09:40:48 +0530

    Day 1 of Learning kotlin

--- src/main/kotlin/Basic.kt
+++ src/main/kotlin/Basic.kt
@@ -9,6 +9,33 @@ fun main(){
     // Type Inference
     val myName1 = "Touseefa Lateef"
     println(myName1)
+
+    // val keyword that can not be reassigned IMMUTABLE
+  /*  val age = 30
+    age = 40
+    val can not be reassigned */
+
+    // var keyword that can be reassigned MUTABLE
+    var  age = 40;
+    age = 30
+    println(age)
+
+    // DataTypes in kotlin
+    /*1.int
+    * 2.Double
+    * 3.Boolean
+    * 4.char
+    * 5.Float
+    * 6.String*/
+
+    var a = 50
+    var pi = 3.14
+    var isSunny = true
+    var first ='A'
+    var value = 3.1457f
+    var b = "Akeel Shafi"
+
+
 }
 
 
