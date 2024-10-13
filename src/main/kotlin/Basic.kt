edaad038feb7706bf49466924ee7f46ba402f6ed
Akeel shafi <akeelshafi20@gmail.com>
2024-10-13 08:16:03 +0530

    Day 1 of Learning kotlin

---
+++ src/main/kotlin/Basic.kt
@@ -0,0 +1,14 @@
+// main function in kotlin
+fun main(){
+    println("Hello World")
+
+    // declaring and initializing a varaible
+    val myName : String ="Akeel Shafi"
+    println(myName)
+
+    // Type Inference
+    val myName1 = "Touseefa Lateef"
+    println(myName1)
+}
+
+
